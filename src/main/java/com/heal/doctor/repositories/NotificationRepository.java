86ede2afd71161fe9dd4a3b67a1babdaf42d4447
shekharkrsingh <shekharkrsingh6432@gmail.com>
2025-10-25 21:10:22 +0530

    Instant.now is removed because it is not required

--- src/main/java/com/heal/doctor/repositories/NotificationRepository.java
+++ src/main/java/com/heal/doctor/repositories/NotificationRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 public interface NotificationRepository extends MongoRepository<NotificationEntity, String> {
-    List<NotificationEntity> findByDoctorIdOrDoctorIdIsNullAndExpiryDateAfterOrderByCreatedAtDesc(String doctorId, Instant now);
+    List<NotificationEntity> findByDoctorIdOrDoctorIdIsNullOrderByCreatedAtDesc(String doctorId);
 
-    List<NotificationEntity> findByIsReadFalseAndDoctorIdOrDoctorIdIsNullAndExpiryDateAfter(String doctorId, Instant now);
+    List<NotificationEntity> findByIsReadFalseAndDoctorIdOrDoctorIdIsNull(String doctorId);
 
-    List<NotificationEntity> findByIsReadFalseAndDoctorIdAndExpiryDateAfter(String doctorId, Instant now);
+    List<NotificationEntity> findByIsReadFalseAndDoctorId(String doctorId);
+
+    Optional<NotificationEntity> findByIdAndDoctorId(String id, String doctorId);
 }
