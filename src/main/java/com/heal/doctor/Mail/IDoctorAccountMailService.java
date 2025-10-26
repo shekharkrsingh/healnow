bda5076eb7bb98938e116a2f6f1f39697f96841d
Shekhar Singh <shekharkrsingh6432@gmail.com>
2025-10-26 11:13:53 +0530

    Typo corrected

--- src/main/java/com/heal/doctor/Mail/IDoctorAccountMailService.java
+++ src/main/java/com/heal/doctor/Mail/IDoctorAccountMailService.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Service;
 public interface IDoctorAccountMailService {
     void doctorWelcomeMail(String doctorName, String email);
     void doctorPasswordChangeMail(String doctorName, String email);
-    void doctorLoginEmailChangedMail(String reciverMail, String doctorName, String doctorOldEmail, String doctorNewEmail );
+    void doctorLoginEmailChangedMail(String receiverMail, String doctorName, String doctorOldEmail, String doctorNewEmail );
 }
