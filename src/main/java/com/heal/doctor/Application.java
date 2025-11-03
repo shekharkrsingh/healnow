9742bac8d899450bbc77f533dcf3738dc39a9cb3
Shekhar Singh <shekharkrsingh6432@gmail.com>
2025-11-03 22:22:03 +0530

    Made changes because create date was not getting auto populated

--- src/main/java/com/heal/doctor/Application.java
+++ src/main/java/com/heal/doctor/Application.java
@@ -1,8 +1,10 @@
 package com.heal.doctor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class Application {
 
 	public static void main(String[] args) {
