a20aaf40cec3c2d43e253294837acaff24a2be30
shekharkrsingh <shekharkrsingh6432@gmail.com>
2025-11-04 23:06:49 +0530

    sending notification through websocket when notificationType is not a system

--- src/main/java/com/heal/doctor/services/impl/NotificationService.java
+++ src/main/java/com/heal/doctor/services/impl/NotificationService.java
@@ -1,11 +1,16 @@
 package com.heal.doctor.services.impl;
 
+import com.heal.doctor.dto.AppointmentDTO;
 import com.heal.doctor.dto.NotificationResponseDTO;
+import com.heal.doctor.dto.WebSocketResponseType;
+import com.heal.doctor.dto.WebsocketResponseDTO;
 import com.heal.doctor.models.NotificationEntity;
+import com.heal.doctor.models.enums.NotificationType;
 import com.heal.doctor.repositories.NotificationRepository;
 import com.heal.doctor.services.INotificationService;
 import com.heal.doctor.utils.CurrentUserName;
 import org.modelmapper.ModelMapper;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -17,16 +22,27 @@ public class NotificationService implements INotificationService {
 
     private final NotificationRepository notificationRepository;
     private final ModelMapper modelMapper;
+    private final SimpMessagingTemplate messagingTemplate;
 
-    public NotificationService(NotificationRepository notificationRepository, ModelMapper modelMapper) {
+
+    public NotificationService(NotificationRepository notificationRepository, ModelMapper modelMapper, SimpMessagingTemplate messagingTemplate) {
         this.notificationRepository = notificationRepository;
         this.modelMapper = modelMapper;
+        this.messagingTemplate = messagingTemplate;
     }
 
     @Override
     public NotificationResponseDTO createNotification(NotificationEntity notification) {
         NotificationEntity savedNotification= notificationRepository.save(notification);
-        return modelMapper.map(savedNotification, NotificationResponseDTO.class);
+        NotificationResponseDTO notificationResponseDTO=modelMapper.map(savedNotification, NotificationResponseDTO.class);
+        if(!notification.getType().equals(NotificationType.SYSTEM)){
+            messagingTemplate.convertAndSend("/topic/appointments/" + CurrentUserName.getCurrentDoctorId(),
+                    WebsocketResponseDTO.<NotificationResponseDTO>builderGeneric()
+                    .type(WebSocketResponseType.NOTIFICATION)
+                    .payload(notificationResponseDTO)
+                    .build());
+        }
+        return notificationResponseDTO;
     }
 
     @Override
