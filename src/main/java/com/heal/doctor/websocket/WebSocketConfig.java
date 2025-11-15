4df01e8ebdedafd481c3e52d0ab57fd4e1a68d48
shekharkrsingh <shekharkrsingh6432@gmail.com>
2025-11-15 15:51:06 +0530

    websocket endpoint is authenticated

--- src/main/java/com/heal/doctor/websocket/WebSocketConfig.java
+++ src/main/java/com/heal/doctor/websocket/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.heal.doctor.websocket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,10 +11,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Autowired
+    private DoctorHandshakeInterceptor doctorHandshakeInterceptor;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
+                .addInterceptors(doctorHandshakeInterceptor)
                 .withSockJS();
     }
 
