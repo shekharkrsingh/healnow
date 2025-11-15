2f99ac946b8f058e0f9fdc3fb7e341b99ba7ad67
Shekhar Singh <shekharkrsingh6432@gmail.com>
2025-11-15 21:04:15 +0530

    In notificationEntity Ther was no proper import of the field presiously add not it is fixed

--- src/main/java/com/heal/doctor/models/NotificationEntity.java
+++ src/main/java/com/heal/doctor/models/NotificationEntity.java
@@ -1,8 +1,10 @@
 package com.heal.doctor.models;
 
 import com.heal.doctor.models.enums.NotificationType;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
