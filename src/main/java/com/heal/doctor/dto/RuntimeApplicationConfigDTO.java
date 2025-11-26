2f04dbfc037b40e7cc32a33d6d321f9bedb3fa25
shekharkrsingh <shekharkrsingh6432@gmail.com>
2025-11-26 22:30:39 +0530

    some un used import is removed

--- src/main/java/com/heal/doctor/dto/RuntimeApplicationConfigDTO.java
+++ src/main/java/com/heal/doctor/dto/RuntimeApplicationConfigDTO.java
@@ -3,7 +3,6 @@ package com.heal.doctor.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 import java.util.Date;
 
--- src/main/java/com/heal/doctor/services/impl/DoctorServiceImpl.java
+++ src/main/java/com/heal/doctor/services/impl/DoctorServiceImpl.java
@@ -31,7 +31,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.print.Doc;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
